559d15bed0e77d3d1b3e897c517849f544e697a2
Adam K <a.kwiatkowski.21@unimail.winchester.ac.uk>
2024-02-27 17:35:23 +0000

    Created client `app.js`

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const apiCall = () => {
+    axios.get('https://localhost:8080').then((data) => {
+      console.log(data)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={apiCall}>Make API Call</button>
       </header>
     </div>
   );
