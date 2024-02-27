6366c3a65cb0982c5033d82f65afcbe2269d50b3
Adam K <a.kwiatkowski.21@unimail.winchester.ac.uk>
2024-02-27 17:39:48 +0000

    installed CORS

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const cors = require('cors');
 
 const express = require('express');
 
 PORT = process.env.PORT || 4000;
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello universe!');
