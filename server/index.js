584ce9709b9852b12935db372e88dc27c6dc5224
Adam K <a.kwiatkowski.21@unimail.winchester.ac.uk>
2024-03-11 15:02:05 +0000

    Initial add of MongoDB stuff

--- server/index.js
+++ server/index.js
@@ -9,6 +9,37 @@ const app = express();
 
 app.use(cors());
 
+
+/// START OF MONGODB TEST BIT
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://avba-host-1:<password>@avba-cluster.lyattit.mongodb.net/?retryWrites=true&w=majority&appName=AVBA-Cluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+/// END OF MONGODB TEST BIT
+
 app.get('/', (req, res) => {
     res.send('Hello universe!');
 });
