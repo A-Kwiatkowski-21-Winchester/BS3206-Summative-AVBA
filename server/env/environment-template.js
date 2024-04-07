1ab437e617313284256a101e0cc49ad8964b9d38
Username <User@Email.com>
2024-04-07 18:42:37 +0100

    Cleared Data for Initial Commit

--- server/env/environment-template.js
+++
@@ -1,9 +0,0 @@
-const clusterName = 'avba-cluster.lyattit.mongodb.net';
-const username = 'INSERT-USERNAME';
-const password = 'INSERT-PASSWORD';
-
-module.exports = {
-  clusterName,
-  username,
-  password,
-};
