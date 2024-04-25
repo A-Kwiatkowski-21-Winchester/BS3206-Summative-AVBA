1772e0212248cab9e6736e4f5f8b0820d4d24c06
B.Gavan <B.Gavan.20@unimail.winchester.ac.uk>
2024-04-26 00:42:46 +0100

    Added framework+CSS to support dropdown within navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../css/navbar.css";
 
-const Navbar = () => {
-    const [isOpen, setOpen] = useState(false);
-    const toggleClass = () => {
 
-    };
+const p = <li>testestestest</li>
+
+const Navbar = () => {
+
     return (
         <nav>
             <div className="navbar">   {/* NavBar links currently link to "placeholder pages". 
@@ -17,6 +17,18 @@ const Navbar = () => {
                 <li><NavLink className="navbar-item" activeClassName="is-active" to="/mental">Mental</NavLink></li>
                 <li><NavLink className="navbar-item" activeClassName="is-active" to="/fitness">Fitness</NavLink></li>
                 <li><NavLink className="navbar-item" activeClassName="is-active" to="/support">Support</NavLink></li>
+                <li>
+                    <div className="dropdown-container">
+                        <NavLink className="dropdown-navbar" to="/">Dropdown</NavLink>
+                        <div className="dropdown-content">
+                            <li>ddd</li>
+                            <li>ccc</li>
+                            <li>bbb</li>
+                        </div>
+
+                    </div>
+
+                </li>
             </div>
         </nav>
     )
