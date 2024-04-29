20640bf3b7c965586ea0cb52bbc8f749732c428a
Adam K <a.kwiatkowski.21@unimail.winchester.ac.uk>
2024-04-30 00:08:45 +0100

    Protect script for npm in root dir
    Only works in Windows (not sure how to write an appropriate script for MacOS)

---
+++ npm.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ________
+echo WARNING^! 
+echo You have executed the [36mnpm[0m command in the root directory, instead of inside the [33mserver[0m or [33mclient[0m directories.
+echo Ensure you use [36mcd[0m to switch into the desired directory before trying again.
