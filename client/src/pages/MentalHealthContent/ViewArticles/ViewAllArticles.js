b8d77afd228f9e7ac2c705ef2316020722a3d32f
AbdullahAshrafWork <abdullah.ashraf@fujitsu.com>
2024-05-07 11:54:16 +0100

    Addes some CSS chnages to view all articles

--- client/src/pages/MentalHealthContent/ViewArticles/ViewAllArticles.js
+++ client/src/pages/MentalHealthContent/ViewArticles/ViewAllArticles.js
@@ -20,25 +20,28 @@ export default function ViewAllArticles(){
         getArticles()
     }, [])
 
-
      function ViewArticle(title, author, date, content){
         setShowAllArticles(false)
 
         setShowSelectedArticle({title, author, date, content})
     }
 
+    function returnToAllArticles(){
+        window.location.reload()
+    }
     
     return(
         <div>
+            <h1 className="title">Mental Health Articles</h1>
             {
                 showAllArticles? 
-                <div className="card">
+                <div className="card">       
                     {articles.map((article) => (
                         <div className="cards">
                             <p key={article._id}>{article.title}</p>
                             <p key={article.author}>By {article.author}</p>
-                            <p key={article.date}>Uploaded: {article.date}</p>
-                            <button onClick={() => ViewArticle(article.title, article.author, article.date, article.content)} >Read More</button>
+                            {/* <p key={article.date}>Uploaded: {article.date}</p> */}
+                            <button className="btn" onClick={() => ViewArticle(article.title, article.author, article.date, article.content)} >Read More</button>
                         </div>
                     ))}
                 </div>
@@ -49,6 +52,7 @@ export default function ViewAllArticles(){
                     <h2>By {showSelectedArticle.author}</h2>
                     <h5>Created: {showSelectedArticle.date}</h5>
                     <p className="content">{showSelectedArticle.content}</p>
+                    <button onClick={returnToAllArticles}>Back</button>
                 </div>
                
 
