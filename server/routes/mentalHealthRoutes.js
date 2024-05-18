d077bc98d9be099ca56450dbc4d3b38e4d5277b6
Adam K <a.kwiatkowski.21@unimail.winchester.ac.uk>
2024-05-18 17:06:31 +0100

    Fixed `mentalHealthRoutes.js` for new client style

--- server/routes/mentalHealthRoutes.js
+++ server/routes/mentalHealthRoutes.js
@@ -8,33 +8,31 @@ const { ObjectId } = require('mongodb');
 
 //Create an article 
 router.post('/create', (req, res) => {
-    dbconnect.generateClient();
-    dbconnect.openClient();
+    let client = dbconnect.generateClient();
+    dbconnect.openClient(client);
    
-    let client = dbconnect.globals.client;
     let db = client.db("MentalHealth")
     let collection = db.collection("Articles")
 
     collection.insertOne(req.body)
     .then((response) => res.status(200).json(response))
     .catch((err) => res.status(500).json(err))
-    .finally(() => dbconnect.closeClient())
+    .finally(() => dbconnect.closeClient(client))
 })
 
 //Get All Articles
 router.get("/", async (_, res) => {
 
-    dbconnect.generateClient();
-    dbconnect.openClient();
+    let client = dbconnect.generateClient();
+    dbconnect.openClient(client);
 
-    let client = dbconnect.globals.client;
     let db = client.db("MentalHealth")
     let collection = db.collection("Articles")
 
     collection.find({}).toArray()
     .then((response) => res.status(200).json(response))
     .catch((err) => res.status(500).json(err))
-    .finally(() => dbconnect.closeClient())
+    .finally(() => dbconnect.closeClient(client))
  
 });
 
